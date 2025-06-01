45fbf898c8f567e89d3a8d9ff1acf6f0ab3965b2
CEDBRASIL <cedbrasil@outlook.com>
2025-06-01 11:02:02 +0000

    dadada

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import structlog
 from cursos import router as cursos_router
 from matricular import router as matricular_router
 from secure import router as secure_router
-from checkout_api import router as checkout_router
 from assinaturamp import router as mp_router
-
+from checkoutteste import router as checkoutteste_router
 
 log = structlog.get_logger()
 
@@ -24,6 +23,7 @@ app.include_router(cursos_router, prefix="/cursos", tags=["Cursos"])
 app.include_router(matricular_router, prefix="/matricular", tags=["Matrícula"])
 app.include_router(secure_router, prefix="/secure", tags=["Token"])
 app.include_router(mp_router, prefix="/assinaturamp", tags=["Assinatura MP"])
+app.include_router(checkoutteste_router, prefix="/checkoutteste", tags=["Checkout Teste"])
 
 
 @app.get("/")
